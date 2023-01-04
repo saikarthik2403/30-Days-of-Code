0e16261fe5ecf64ce964ffd64f7ba428d43bee4d
saikarthik2403 <111523220+saikarthik2403@users.noreply.github.com>
2023-01-04 23:06:53 +0530

    Create Java Int to String.java

---
+++ Java Int to String.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+        String s = String.valueOf(n);
+            System.out.println("Good job");
+    }
+}
