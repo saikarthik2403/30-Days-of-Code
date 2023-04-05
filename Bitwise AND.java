dce1b53e880366ccd7b7b9c378de6f9fce0b1f4a
Sai karthik <111523220+saikarthik2403@users.noreply.github.com>
2023-04-05 20:07:09 +0530

    Create Bitwise AND.java

---
+++ Bitwise AND.java
@@ -0,0 +1,117 @@
+//Bitwise AND
+//https://www.hackerrank.com/challenges/30-bitwise-and/problem
+/*
+Task
+Given set . Find two integers,  and  (where ), from set  such that the value of  is the maximum possible and also less than a given integer, . In this case,  represents the bitwise AND operator.
+
+Function Description
+Complete the bitwiseAnd function in the editor below.
+
+bitwiseAnd has the following paramter(s):
+- int N: the maximum integer to consider
+- int K: the limit of the result, inclusive
+
+Returns
+- int: the maximum value of  within the limit.
+
+Input Format
+
+The first line contains an integer, , the number of test cases.
+Each of the  subsequent lines defines a test case as  space-separated integers,  and , respectively.
+
+Constraints
+
+Sample Input
+
+STDIN   Function
+-----   --------
+3       T = 3
+5 2     N = 5, K = 2
+8 5     N = 8, K = 5
+2 2     N = 8, K = 5
+Sample Output
+
+1
+4
+0
+Explanation
+
+ 
+
+All possible values of  and  are:
+
+The maximum possible value of  that is also  is , so we print  on a new line.
+*/
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'bitwiseAnd' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER N
+     *  2. INTEGER K
+     */
+
+    public static int bitwiseAnd(int N, int K) {
+    // Write your code here
+    int result = 0;
+    for(int i=0;i<N;i++){
+        for(int j=i+1;j<=N;j++){
+            int val = i&j;
+            if(val<K && val>result){
+                result = val;
+            }
+        }
+    }
+    return result;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int count = Integer.parseInt(firstMultipleInput[0]);
+
+                int lim = Integer.parseInt(firstMultipleInput[1]);
+
+                int res = Result.bitwiseAnd(count, lim);
+
+                bufferedWriter.write(String.valueOf(res));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+
